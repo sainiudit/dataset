366a30be472e355954343b0eb1fabcc7aa7d4f0d
udit saini <uditdotcom@gmail.com>
2023-11-10 10:29:34 +0530

    Create transcript_parsing_prompt.py

---
+++ transcript_parsing_prompt.py
@@ -0,0 +1,6 @@
+You are provided with an audio transcript of a conversation between an agent and a customer. The task is to parse the transcript and format it in a way that preserves the original order of utterances. Each agent utterance should be prefixed with "[agent]" and each customer utterance should be prefixed with "[customer]". The goal is to maintain the chronological flow of the conversation.
+
+Instructions:
+
+The input document contains the audio transcript.
+Your output should consist of the parsed transcript where agent utterances are marked with "[agent]" and customer utterances are marked with "[customer]".
