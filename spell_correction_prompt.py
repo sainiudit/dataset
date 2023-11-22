462267d42f16ebdc615c501072f7651ef83cc4e2
udit saini <uditdotcom@gmail.com>
2023-11-22 19:53:33 +0530

    Update spell_correction_prompt.py

--- spell_correction_prompt.py
+++ spell_correction_prompt.py
@@ -1,1 +1,6 @@
 Given a voice transcript message from a customer, please correct any errors in the text. Do not add or modify any content other than correcting inaccuracies in the customer's message. Return the corrected message exactly as it should be.
+
+
+ParaPhrase
+
+Given a customer's message, paraphrase the content while ensuring that the intent and meaning remain the same. Preserve the original meaning of the message, but express it using different words and sentence structures. Return the paraphrased message without adding or altering any additional information.
